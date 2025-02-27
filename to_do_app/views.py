c378e06ddcc206c671cc0cbd27c83321fddf9532
amangaliev ersyn <amangalieversyn@gmail.com>
2025-02-27 17:39:57 +0500

    Добавлены API-представления для списка, создания и просмотра задачи

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render ,get_object_or_404,redirect,reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Task
+from .serializers import TaskSerializer
+from rest_framework import generics
+
+
+class TaskListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
 
 def index_view(request):
     tasks = Task.objects.all()
     return render(request, "index.html", {"tasks": tasks})
 
+
 def task_list_view(request):
     tasks = Task.objects.all()
     return render(request, 'task_list.html', {'tasks': tasks})
 
+
 def task_detail(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     return render(request, "task_detail.html", {"task": task})
+
 
 def add_task_view(request):
     if request.method == 'POST':
@@ -31,8 +47,6 @@ def add_task_view(request):
         return redirect(reverse('task_list'))
 
     return render(request, 'task_form.html')
-
-
 
 
 def delete_task_view(request, task_id):
