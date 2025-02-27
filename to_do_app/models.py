3d8d20a1029129370599ec947d6f39ad4dd49385
amangaliev ersyn <amangalieversyn@gmail.com>
2025-02-27 11:34:55 +0500

    Обновил модели скопировал view с предущего проекта и создал шаблоны без наполнения

--- to_do_app/models.py
+++ to_do_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Task(models.Model):
     STATUS_CHOICES = [
         ('new', 'Новая'),
@@ -8,7 +7,13 @@ class Task(models.Model):
         ('completed', 'Завершена'),
     ]
 
-    title = models.TextField(max_length=2000,null=True,blank=True)
-    description = models.TextField(max_length=2000,null=False,blank=False)
-    status = models.CharField(max_length=20,null=False,blank=False,default='new',choices=STATUS_CHOICES)
-    due_date = models.DateField(null=True,blank=True,default=None)
+    title = models.TextField(max_length=2000, null=False, blank=False,default='No title')
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='new',
+        null=False,
+        blank=False
+    )
+    due_date = models.DateField(null=True, blank=True)
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
+from .models import Task
 
-# Create your views here.
+def index_view(request):
+    return render(request, 'index.html')
+
+def task_list_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks_list.html', {'tasks': tasks})
+
+
+def add_task_view(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status', 'new')
+        date = request.POST.get('due_date')
+
+        Task.objects.create(
+            description=description,
+            status=status,
+            data=date,
+        )
+
+        tasks = Task.objects.all()
+        return render(request, "tasks_list.html", {"tasks": tasks})
+
+    return render(request, 'task_form.html')
+
+
+
+
+def delete_task_view(request, task_id):
+    task = Task.objects.filter(id=task_id).first()  # Ищем задачу, но не вызываем 404
+    if task:
+        task.delete()
+
+    tasks = Task.objects.all()  # Получаем обновленный список задач
+    return render(request, 'tasks_list.html', {'tasks': tasks})
